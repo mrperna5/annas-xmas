d94753ad30a64eac9340c74c8b1c747e7ec52da7
PERNAG3\perna <pernamichael55@gmail.com>
2022-12-24 19:12:13 +0100

    xmas eve 5

--- src/app/mock-secrets.ts
+++ src/app/mock-secrets.ts
@@ -1,12 +1,13 @@
 import { Secret } from './secret'
 
 export const SECRETS: Secret[] = [
-    { id: 1, password: 'JAKOB', inputPass: '', message: 'xm<sup>2</sup> = Besitzer (x, Monat, Jahr)', passwordCounter: 0 },
-    { id: 2, password: 'B123', inputPass: '', message: 'Secret Message two', passwordCounter: 0 },
-    { id: 3, password: 'c123', inputPass: '', message: 'Secret Message three', passwordCounter: 0 },
-    { id: 4, password: 'd123', inputPass: '', message: 'Secret Message four', passwordCounter: 0 },
-    { id: 5, password: 'e123', inputPass: '', message: 'Secret Message five', passwordCounter: 0 },
-    { id: 6, password: 'f123', inputPass: '', message: 'Secret Message six', passwordCounter: 0 },
-    { id: 7, password: 'g123', inputPass: '', message: 'Secret Message seven', passwordCounter: 0 },
-    { id: 8, password: 'h123', inputPass: '', message: 'Secret Message eight', passwordCounter: 0 },
+    { id: 1, password: 'OPTI', inputPass: '', message: 'xm<sup>2</sup> = Besitzer (x, Monat, Jahr)', passwordCounter: 0 },
+    { id: 2, password: '489', inputPass: '', message: '', passwordCounter: 0 },
+    { id: 3, password: '20439', inputPass: '', message: '', passwordCounter: 0 },
+    { id: 4, password: '3935', inputPass: '', message: 'das nächste rätsel liegt nicht fern, im raum nämlich, den ihr beide habt gern. Durch gläserne türe tretet nur ein. Wie schön sind die Bilder von euren Kindern, den zwein.', passwordCounter: 0 },
+    { id: 5, password: 'SCHNUFFI', inputPass: '', message: 'Das nächste Rätsel wartet auf euch in einem Raume, der steinern und feucht. Wie die Zentrale der FDP bietet auch dieser Raum vielen Flaschen Obdach.', passwordCounter: 0 },
+    { id: 6, password: 'SIMSALABIM', inputPass: '', message: 'Lang hing ein Clown von Sieglinde in diesem Raume im Winde.', passwordCounter: 0 },
+    { id: 7, password: 'GLADIOLEN', inputPass: '', message: '', passwordCounter: 0 },
+    { id: 8, password: 'ANNA', inputPass: '', message: 'Hinweis für Jakob Zimmer: Brumm, brumm, brumm, ich bin der Bär. Brumm, brumm, brumm und bin ziemlich schwer. So reimte einst ein junges Mädchen, so lieb und klug und einfallsreich. Sie spielte gerne Einkaufslädchen, Tiger und Löwe, auch manchen Streich. Den nächsten Raum nun suchet auf, wo schon so manches nahm seinen Lauf. Es gab mal eine kurze Ära, da wurde jener Raum zur Anlaufstelle aller mit einem Lesetraum. Man musst sich registrieren, es galt auch Buch zu führen und gab mans Buch nicht gleich zurück, dann gab es Strafgebühren. Nun? Wisst ihr schon, von wem ich spreche? Für sie ich stets ne Lanze breche.', passwordCounter: 0 },
+    { id: 9, password: 'FINALE', inputPass: '', message: 'GENERATOR P.M. Nutzungshinsweise:<br>Lage: 809 m ü. NHN<br>Temperatur: zwischen 40-120 Grad<br>', passwordCounter: 0 }
 ]
